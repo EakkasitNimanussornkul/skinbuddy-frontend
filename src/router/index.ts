d2ae1b9651ebfaa215eef02175ae27b74c512f6f
teeranate <teeranate_tho@cmu.ac.th>
2026-05-07 01:53:03 +0700

    update(router): add new route for quiz and homeview

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SkinQuizView from '../views/SkinQuizView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/quiz',
+      name: 'quiz',
+      component: SkinQuizView
+    }
+  ]
 })
 
 export default router
