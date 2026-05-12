bfcb4b847b7d4a7ca31211d0517c409478c05889
teeranate <teeranate_tho@cmu.ac.th>
2026-05-13 03:58:59 +0700

    update(router): add new route for Explore and Routine Placeholder and add Auth for required feature

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,8 @@ import HomeView from '../views/HomeView.vue'
 import SkinQuizView from '../views/SkinQuizView.vue'
 import ShelfView from '../views/ShelfView.vue'
 import SettingsView from '../views/SettingsView.vue'
+import ExploreView from '../views/ExploreView.vue'
+import RoutineView from '../views/RoutineView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -33,13 +35,25 @@ const router = createRouter({
       path: '/shelf',
       name: 'shelf',
       component: ShelfView,
+      meta: { requiresAuth: true }
     },
     {
-  path: '/settings',
-  name: 'settings',
-  component: SettingsView,
-  meta: { requiresAuth: true }
-}
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/explore',
+      name: 'explore',
+      component: ExploreView
+    },
+    {
+      path: '/routine',
+      name: 'routine',
+      component: RoutineView,
+      meta: { requiresAuth: true }
+    }
   ],
 })
 // If you want the any link to be protected, write the line below:
