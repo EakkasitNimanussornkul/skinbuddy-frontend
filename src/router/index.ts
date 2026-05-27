98b5fb2f33f86df8678011ce3f5720b87baa2649
Eakkasit Nimanussornkul <eakkasitnim@gmail.com>
2026-05-27 09:42:50 +0700

    feat(chatbot): added link to chatbot in the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 import AuthCallbackView from '../views/AuthCallbackView.vue'
+import ChatbotView from '../views/ChatbotView.vue'
 import ErrorView from '../views/ErrorView.vue'
-import HomeView from '../views/HomeView.vue'
-import SkinQuizView from '../views/SkinQuizView.vue'
-import ShelfView from '../views/ShelfView.vue'
-import SettingsView from '../views/SettingsView.vue'
 import ExploreView from '../views/ExploreView.vue'
+import HomeView from '../views/HomeView.vue'
 import RoutineView from '../views/RoutineView.vue'
+import SettingsView from '../views/SettingsView.vue'
+import ShelfView from '../views/ShelfView.vue'
+import SkinQuizView from '../views/SkinQuizView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -35,25 +37,31 @@ const router = createRouter({
       path: '/shelf',
       name: 'shelf',
       component: ShelfView,
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
     {
       path: '/settings',
       name: 'settings',
       component: SettingsView,
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
     {
       path: '/explore',
       name: 'explore',
-      component: ExploreView
+      component: ExploreView,
     },
     {
       path: '/routine',
       name: 'routine',
       component: RoutineView,
-      meta: { requiresAuth: true }
-    }
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: ChatbotView,
+      meta: { requiresAuth: true },
+    },
   ],
 })
 // If you want the any link to be protected, write the line below:
