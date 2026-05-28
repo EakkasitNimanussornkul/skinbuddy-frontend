c6258333768f612e05656b842b9d0e6352377423
Eakkasit Nimanussornkul <eakkasitnim@gmail.com>
2026-05-28 17:38:25 +0700

    fix(app): remove duplicated script that happend when merged

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,13 @@
 import { onMounted } from 'vue'
 import { RouterView, useRoute } from 'vue-router'
 import BottomNav from './components/BottomNav.vue'
-import ToastProvider from './components/ToastProvider.vue'
-import { useThemeStore } from './stores/themeStore'
-// 1. Import the new popup component
 import LoginPopup from './components/LoginPopup.vue'
-
-const themeStore = useThemeStore()
-const route = useRoute()
-
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import { RouterView, useRoute } from 'vue-router'
-import BottomNav from './components/BottomNav.vue'
-import { useThemeStore } from './stores/themeStore'
 import ToastProvider from './components/ToastProvider.vue'
+import { useThemeStore } from './stores/themeStore'
+
 const themeStore = useThemeStore()
 const route = useRoute()
+
 
 onMounted(() => {
   themeStore.initTheme()
