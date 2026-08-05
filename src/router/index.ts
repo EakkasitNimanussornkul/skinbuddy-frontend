7358b1ab70959d2533b07ec5308b63f73a8d5c30
teeranate <teeranate_tho@cmu.ac.th>
2026-08-05 13:28:04 +0700

    feat(router): add scroll behavior to reset the position of the page

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,13 @@ import WeeklyCheckInView from '../views/WeeklyCheckInView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0, behavior: 'smooth' }
+    }
+  },
   routes: [
     {
       path: '/',
